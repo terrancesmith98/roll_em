a240d3d397cca60635a2f847d1ecfd4c81672039
Terry Smith <terrancesmith98@gmail.com>
2016-09-09 10:09:30 -0400

    Updated H1 Styles

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,15 +1,2 @@
 
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-
-
-function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-var user = { firstName: "Jane", lastName: "User" };
-
-document.body.innerHTML = greeter(user);   
+ 
