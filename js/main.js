7ff0ca0ac5956d2cdd2b3985e1789d6140b3ba50
tjsmith98 <terrancesmith98@gmail.com>
2016-09-12 22:48:31 -0400

    moved 3d canvas - worked on roll method

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-var Die;
+var Die,
+  slice = [].slice;
 
 Die = (function() {
   function Die(sides) {
     this.sides = sides;
   }
 
-  Die.prototype.roll = function(quantity) {
-    var result;
-    result = Math.floor(Math.random() * this.sides + 1) * quantity;
+  Die.prototype.roll = function() {
+    var quantity, result;
+    quantity = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+    result = [];
+    quantity.forEach(function() {
+      return result = Math.floor(Math.random() * this.sides + 1);
+    });
     return result;
   };
 
--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -1,9 +1,11 @@
 class Die
   constructor: (@sides) ->
 
-  roll: (quantity) ->
+  roll: (quantity...) ->
     #alert "You rolled #{quantity} #{@sides} sided dice."
-    result = Math.floor((Math.random() * @sides + 1)) * quantity
+    result = [];
+    quantity.forEach () ->
+      result = Math.floor((Math.random() * @sides + 1))
     result
 
 
